f3d24ff3b922cbf2ef11c9c109f257f9964b5fe6
Cristian Angel Puma Villalva <cristian.puma.es6@gmail.com>
2022-01-15 02:17:08 -0500

    Orden y mas orden

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,117 +2,138 @@
 <html lang="en">
 
 <head>
-    <title>Login V10</title>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!--===============================================================================================-->
-    <link rel="icon" type="image/png" href="templatelogin/images/icons/favicon.ico" />
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="templatelogin/vendor/bootstrap/css/bootstrap.min.css">
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="templatelogin/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="templatelogin/fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="templatelogin/vendor/animate/animate.css">
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="templatelogin/vendor/css-hamburgers/hamburgers.min.css">
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="templatelogin/vendor/animsition/css/animsition.min.css">
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="templatelogin/vendor/select2/select2.min.css">
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="templatelogin/vendor/daterangepicker/daterangepicker.css">
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="templatelogin/css/util.css">
-    <link rel="stylesheet" type="text/css" href="templatelogin/css/main.css">
-    <!--===============================================================================================-->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Plataforma de Empleo Inclusivo</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+
+    <style>
+    .divider:after,
+    .divider:before {
+        content: "";
+        flex: 1;
+        height: 1px;
+        background: #eee;
+    }
+
+    .h-custom {
+        height: calc(100% - 73px);
+    }
+
+    @media (max-width: 450px) {
+        .h-custom {
+            height: 100%;
+        }
+    }
+    </style>
 </head>
 
 <body>
 
-    <div class="limiter">
-        <div class="container-login100">
-            <div class="wrap-login100 p-t-50 p-b-90">
-
-                <form class="login100-form validate-form flex-sb flex-w" method="POST" action="{{ route('login') }}">
-                    @csrf
-                    <span class="login100-form-title p-b-51">
-                        {{ __('Login') }}
-                    </span>
 
 
+    <section class="vh-100">
+        <div class="container-fluid h-custom">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col-md-9 col-lg-6 col-xl-5">
+                    <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+                        class="img-fluid" alt="Sample image">
+                </div>
+                <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
 
-                    <div class="wrap-input100 validate-input m-b-16" data-validate="Username is required">
-                        <input id="email" type="email" class="form-control input100 @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="{{ __('E-Mail Address') }}" autofocus>
+                    <form class="login100-form validate-form flex-sb flex-w" method="POST"
+                        action="{{ route('login') }}">
 
-                        @error('email')
-                        <span class="focus-input100 invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                        @enderror
-                    </div>
-
-
-                    <div class="wrap-input100 validate-input m-b-16" data-validate="Password is required">
-                        <input id="password" class="form-control input100 @error('password') is-invalid @enderror" type="password" name="password" placeholder="{{ __('Password') }} " required autocomplete="current-password">
-
-                        @error('password')
-                        <span class="focus-input100 invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                        @enderror
-                    </div>
-
-
-                    <div class="flex-sb-m w-full p-t-3 p-b-24">
-                        <div class="contact100-form-checkbox">
-                            <input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me">
-                            <label class="label-checkbox100" for="ckb1">
-                                {{ __('Remember Me') }}
-                            </label>
+                        @csrf<div
+                            class="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
+                            <p class="lead fw-normal mb-0 me-3">Inicia Session</p>
                         </div>
 
-                        <div>
-                            @if (Route::has('password.request'))
-                            <a href="{{ route('password.request') }}" class="txt1 btn btn-link">
-                                {{ __('Forgot Your Password?') }}
-                            </a>
-                            @endif
+                        <div class="divider d-flex align-items-center my-4">
                         </div>
-                    </div>
 
-                    <div class="container-login100-form-btn m-t-17">
-                        <button type="submit" class="login100-form-btn">
-                            {{ __('Login') }}
-                        </button>
-                    </div>
+                        <!-- Email input -->
+                        <div class="form-outline mb-4 validate-input m-b-16" data-validate="Requiere ingresar un Email">
+                            <input id="email" type="email"
+                                class="form-control form-control-lg @error('email') is-invalid @enderror" name="email"
+                                value="{{ old('email') }}" required autocomplete="email"
+                                placeholder="{{ __('Ingrese E-Mail') }}" autofocus>
+
+                            @error('email')
+                            <span class="focus-input100 invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
+                        </div>
+
+                        <!-- Password input -->
+                        <div class="form-outline mb-3" validate-input m-b-16"
+                            data-validate="Requiere ingresar una contraseña">
+                            <input id="password"
+                                class="form-control  form-control-lg @error('password') is-invalid @enderror"
+                                type="password" name="password" placeholder="{{ __('Password') }} " required
+                                autocomplete="current-password">
+
+                            @error('password')
+                            <span class="focus-input100 invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
+                        </div>
+
+                        <div class="d-flex justify-content-between align-items-center">
+                            <!-- Checkbox -->
+                            <div class="form-check mb-0">
+                                <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3" />
+                                <label class="form-check-label" for="form2Example3">
+                                    Recordar session
+                                </label>
+                            </div>
+                            <a href="#!" class="text-body">Forgot password?</a>
+                        </div>
+
+                        <div class="text-center text-lg-start mt-4 pt-2">
 
 
-                </form>
+                            <button type="submit" class="btn btn-primary btn-lg"
+                                style="padding-left: 2.5rem; padding-right: 2.5rem;">
+                                {{ __('Login') }}
+                            </button>
+                            <p class="small fw-bold mt-2 pt-1 mb-0">¿No tienes una cuenta?
+                                @if (Route::has('register'))
+                                <a class="link-danger" href="{{ route('register') }}">Registrate</a>
+                                @endif
+                        </div>
+
+                    </form>
+                </div>
             </div>
         </div>
-    </div>
+        <div
+            class="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-primary">
+            <!-- Copyright -->
+            <div class="text-white mb-3 mb-md-0">
+                Copyright © 2022. Todos los derechos reservados
+            </div>
+            <!-- Copyright -->
+
+            <!-- Right -->
+            <div>
+            </div>
+            <!-- Right -->
+        </div>
+    </section>
 
 
-    <div id="dropDownSelect1"></div>
 
-    <!--===============================================================================================-->
-    <script src="templatelogin/vendor/jquery/jquery-3.2.1.min.js"></script>
-    <!--===============================================================================================-->
-    <script src="templatelogin/vendor/animsition/js/animsition.min.js"></script>
-    <!--===============================================================================================-->
-    <script src="templatelogin/vendor/bootstrap/js/popper.js"></script>
-    <script src="templatelogin/vendor/bootstrap/js/bootstrap.min.js"></script>
-    <!--===============================================================================================-->
-    <script src="templatelogin/vendor/select2/select2.min.js"></script>
-    <!--===============================================================================================-->
-    <script src="templatelogin/vendor/daterangepicker/moment.min.js"></script>
-    <script src="templatelogin/vendor/daterangepicker/daterangepicker.js"></script>
-    <!--===============================================================================================-->
-    <script src="templatelogin/vendor/countdowntime/countdowntime.js"></script>
-    <!--===============================================================================================-->
-    <script src="templatelogin/js/main.js"></script>
+
+    <!--Footer-->
+
+
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
 
 </body>
 
